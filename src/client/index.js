a3bd72057263d6ede850852e578c14f3c1328e6f
ballihakkari <ballibronze@gmail.com>
2018-10-22 17:51:32 +0000

    Now when a player wins, their score is incremented

--- src/client/index.js
+++ src/client/index.js
@@ -2,11 +2,15 @@ const css = require('../css/app.css');
 const hasWon = require("../logic/hasWon");
 const tictactoe = require("../logic/tictactoe");
 const sumOf2D = require("../logic/sumOf2D");
-const boardInsert = require("../logic/boardInsert")
+const boardInsert = require("../logic/boardInsert");
+const increaseScore = require("../logic/increaseScore");
 
 var moveNr = 0;
 var board = [[0,0,0],[0,0,0],[0,0,0]]; 
 var htmlBoard = document.getElementsByClassName('cell');
+var xScore = document.getElementById('XplayerScoreDisplay');
+var yScore = document.getElementById('YplayerScoreDisplay');
+
 /*
 for(var i = 0; i < 9; i++) {
 	htmlBoard[i].addEventListener("click", function() {ticTacToe(floor(i/3), (i%3));});
@@ -25,12 +29,12 @@ htmlBoard[8].addEventListener("click", function() {playerMove(2, 2);});
 
 function playerMove(row, col) {
   var moveMade = tictactoe(row, col, board, htmlBoard, moveNr);
+  moveNr++;
   if(moveMade = true)
   {
-    moveNr++;
     boardInsert(row, col, htmlBoard, moveNr);
     if(hasWon(board)) {
-      console.log("Player " + (moveNr%2+1) +" has won");
+      increaseScore(xScore, yScore, moveNr);
       reset();
     }
     if(moveNr == 9)
