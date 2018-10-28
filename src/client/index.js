ddab22349cc5f2276bb3d7cb2ad9c5bb0a12f2a8
ballihakkari <ballibronze@gmail.com>
2018-10-28 18:50:37 +0000

    refactored to work

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,5 @@
-//const css = require('../css/app.css');
 const tictactoe = require("../logic/tictactoe");
-const increaseScore = require("../logic/increaseScore");
 const setPlayerMove = require("../logic/setPlayerMove");
-
 var moveNr = 0;
 var hasWon = false;
 var board = [[0,0,0],[0,0,0],[0,0,0]]; 
@@ -10,6 +7,12 @@ var htmlBoard = document.getElementsByClassName('cell');
 var xScore = document.getElementById('XplayerScoreDisplay');
 var yScore = document.getElementById('YplayerScoreDisplay');
 var playerTurnDisplay = document.getElementById('playerTurnDisplay');
+
+function fetchHasWon(board)
+{
+  
+}
+
 
 
 htmlBoard[0].addEventListener("click", function() {playerMove(0, 0);});
@@ -22,10 +25,11 @@ htmlBoard[6].addEventListener("click", function() {playerMove(2, 0);});
 htmlBoard[7].addEventListener("click", function() {playerMove(2, 1);});
 htmlBoard[8].addEventListener("click", function() {playerMove(2, 2);});
 
-function playerMove(row, col) {
+async function playerMove(row, col) {
   var moveMade = tictactoe(row, col, board, moveNr);
   if(moveMade == true)
   {
+    console.log(hasWon);
     moveNr++;
       fetch("/api/boardInsert/"+moveNr)
       .then(function(res){
@@ -34,16 +38,14 @@ function playerMove(row, col) {
       .then(function(data){
         htmlBoard[row*3+col].innerHTML = data.boardInsert;
       });
-      console.log(board[0][0]);
-      fetch("/api/hasWonAPI/"+board[0][0]+"/"+board[0][1]+"/"+board[0][2]+"/"+board[1][0]+"/"+board[1][1]+"/"+board[1][2]+"/"+board[2][0]+"/"+board[2][1]+"/"+board[2][2])
-      .then(function(res){
-        return res.json();
-      })
-      .then(function(data){
-        console.log(data);
-        hasWon = data.hasWonAPI;
-      })
-      if(hasWon || moveNr == 9)
+  await fetch("/api/hasWonAPI/"+board[0][0]+"/"+board[0][1]+"/"+board[0][2]+"/"+board[1][0]+"/"+board[1][1]+"/"+board[1][2]+"/"+board[2][0]+"/"+board[2][1]+"/"+board[2][2])
+  .then(function(res){
+    return res.json();
+  })
+  .then(function(data){
+    hasWon = data.hasWonAPI;
+  });
+      if( hasWon || moveNr == 9)
       {
         fetch("/api/reset")
         .then(function(res){
@@ -56,6 +58,7 @@ function playerMove(row, col) {
             htmlBoard[i].innerHTML = "";
           }
         });
+        return;
       }
     setPlayerMove(playerTurnDisplay, moveNr);
 }
--- src/logic/convertToBoard.js
+++ src/logic/convertToBoard.js
@@ -1,6 +1,13 @@
 
 function convertToBoard(field0, field1, field2, field3, field4, field5, field6, field7, field8){
     var board = [[field0,field1,field2],[field3,field4,field5],[field6,field7,field8]]; 
+    for(i = 0; i<board.length; i++)
+    {
+        for(j = 0; j<board[i].length; j++)
+        {
+            board[i][j] = parseInt(board[i][j])
+        }
+    }
     return board;
 }
 module.exports = convertToBoard;
