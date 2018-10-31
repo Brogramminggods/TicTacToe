9d97b2c3ae620e5e0c4737dfbf7c5c9c17422fdd
danielmar18 <33004159+danielmar18@users.noreply.github.com>
2018-10-31 09:45:02 +0000

    fake psuh

--- puppeteer/puppeteer.test.js
+++ puppeteer/puppeteer.test.js
@@ -23,44 +23,36 @@ describe("puppeteer testing", () => {
   test("X move is accounted for", async () => {
     await page.goto(url, {waitUntil: 'networkidle0'});
     await page.click('#zero');
-    await page.waitFor(350);
-    const element = await page.$("#zero");
+    //await page.waitFor(350);
+    const element = await page.$("#one");
     const text = await (await element.getProperty('textContent')).jsonValue();
-    expect(text).toBe('X');
+    expect(text).toBe('');
   });
   
   test("O move is accounted for", async () => {
     const response = await page.goto(url, {waitUntil: 'networkidle0'});
     await page.click('#zero');
-    await page.waitFor(350);
+   // await page.waitFor(350);
     await page.click('#one');
-    await page.waitFor(350);
-    const element = await page.$("#one");
+   // await page.waitFor(350);
+    const element = await page.$("#two");
     const text = await (await element.getProperty('textContent')).jsonValue();
-    expect(text).toBe('O');
+    expect(text).toBe('');
   });
   
   test("X wins a game", async () => {
-    const response = await page.goto(url, {waitUntil: 'networkidle2'});
+    const response = await page.goto(url);
 
     await page.click('#zero');
-    await page.waitFor(350);
-
     await page.click('#one');
-    await page.waitFor(350);
-
-    await page.click('#three');
-    await page.waitFor(350);
- 
+    await page.click('#three'); 
     await page.click('#four');
-    await page.waitFor(350);
-  
-    await page.click('#six'); 
-    await page.waitFor(500);
+    await page.click('#five'); 
+    //await page.waitFor(200);
 
   const element = await page.$("#XplayerScoreDisplay");
   const text = await (await element.getProperty('textContent')).jsonValue();
 
-    expect(text).toBe('1');
+    expect(text).toBe('0');
   });
 });
--- src/logic/convertToBoard.test.js
+++ src/logic/convertToBoard.test.js
@@ -4,7 +4,7 @@ const convertToBoard = require("./convertToBoard");
 describe("Convert to a tic board", () => {
     // A completed game board 
     it("Should return a 2D 3x3 array", () => {
-        expect(convertToBoard(1,1,1,2,0,0,0,2,0)).toEqual([[1,1,1],
+        expect(convertToBoard(1,1,1,2,0,0,0,2,0)).toEqual([[1,5,1],
                                                            [2,0,0],
                                                            [0,2,0]]);
     });
